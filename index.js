7f724389ef3cdb2e48651a1535e2efb59b15a99b
Rich Trott <rtrott@gmail.com>
2022-01-23 09:08:34 -0800

    fix: make sure it works as a CLI

--- index.js
+++ index.js
@@ -1,3 +1,5 @@
+#!/usr/bin/env node
+
 'use strict';
 
 if (!process.argv[2]) {
--- test.js
+++ test.js
@@ -4,6 +4,6 @@ const assert = require('assert');
 const { spawnSync } = require('child_process');
 
 {
-  const { stdout } = spawnSync(process.execPath, ['./index.js', 'http://neverssl.com']);
+  const { stdout } = spawnSync('./index.js', ['http://neverssl.com']);
   assert.ok(stdout.toString().includes("And if the network never redirects you to this page, well as you can see, you're not missing much."));
 }
