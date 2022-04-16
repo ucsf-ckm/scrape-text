95aeb38085544838689bda5887e54ad383c950b5
Rich Trott <rtrott@gmail.com>
2022-04-15 17:57:22 -0700

    chore: use TypeScript
    BREAKING CHANGE: drop support for Node.js 12.x

--- index.js
+++ index.js
@@ -1,25 +1,19 @@
 #!/usr/bin/env node
-
-'use strict';
-
-if (!process.argv[2]) {
-  console.error('Usage: scrape-text <URL>');
-  process.exit(1);
+import puppeteer from 'puppeteer';
+const urlToScrape = process.argv[2];
+if (urlToScrape === undefined || urlToScrape === '') {
+    console.error('Usage: scrape-text <URL>');
+    process.exit(1);
 }
-
-const puppeteer = require('puppeteer');
-
-(async () => {
-  const browser = await puppeteer.launch();
-  const page = await browser.newPage();
-  const headlessUserAgent = await page.evaluate(() => navigator.userAgent);
-  const chromeUserAgent = headlessUserAgent.replace('HeadlessChrome', 'Chrome');
-  await page.setUserAgent(chromeUserAgent);
-  await page.setExtraHTTPHeaders({
+const browser = await puppeteer.launch();
+const page = await browser.newPage();
+const headlessUserAgent = await page.evaluate(() => navigator.userAgent);
+const chromeUserAgent = headlessUserAgent.replace('HeadlessChrome', 'Chrome');
+await page.setUserAgent(chromeUserAgent);
+await page.setExtraHTTPHeaders({
     'accept-language': 'en-US,en;q=0.8'
-  });
-  await page.goto(process.argv[2]);
-  const name = await page.$eval('body', el => el.innerText);
-  console.log(name);
-  await browser.close();
-})();
+});
+await page.goto(urlToScrape);
+const name = await page.$eval('body', el => el.innerText);
+console.log(name);
+await browser.close();
