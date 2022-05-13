3eaaf417f4789c23e366472c7749965dab6f16c9
Rich Trott <rtrott@gmail.com>
2022-05-12 17:26:47 -0700

    chore: remove TypeScript, update test matrix for Node.js 18.x

--- index.js
+++ index.js
@@ -1,19 +1,24 @@
 #!/usr/bin/env node
-import puppeteer from 'puppeteer';
-const urlToScrape = process.argv[2];
+
+import puppeteer from 'puppeteer'
+
+const urlToScrape = process.argv[2]
 if (urlToScrape === undefined || urlToScrape === '') {
-    console.error('Usage: scrape-text <URL>');
-    process.exit(1);
+  console.error('Usage: scrape-text <URL>')
+  process.exit(1)
 }
-const browser = await puppeteer.launch();
-const page = await browser.newPage();
-const headlessUserAgent = await page.evaluate(() => navigator.userAgent);
-const chromeUserAgent = headlessUserAgent.replace('HeadlessChrome', 'Chrome');
-await page.setUserAgent(chromeUserAgent);
+
+const browser = await puppeteer.launch()
+const page = await browser.newPage()
+const headlessUserAgent = await page.evaluate(() => navigator.userAgent)
+const chromeUserAgent = headlessUserAgent.replace('HeadlessChrome', 'Chrome')
+
+await page.setUserAgent(chromeUserAgent)
 await page.setExtraHTTPHeaders({
-    'accept-language': 'en-US,en;q=0.8'
-});
-await page.goto(urlToScrape);
-const name = await page.$eval('body', el => el.innerText);
-console.log(name);
-await browser.close();
+  'accept-language': 'en-US,en;q=0.8'
+})
+await page.goto(urlToScrape)
+
+const bodyText = await page.$eval('body', el => el.innerText)
+console.log(bodyText)
+await browser.close()
--- test.js
+++ test.js
@@ -1,11 +1,12 @@
-'use strict';
-import assert from 'node:assert';
-import { spawnSync } from 'node:child_process';
+import assert from 'node:assert'
+import { spawnSync } from 'node:child_process'
+
 {
-    const { stdout } = spawnSync('./index.js', ['http://neverssl.com']);
-    assert.ok(stdout.toString().includes("And if the network never redirects you to this page, well as you can see, you're not missing much."));
+  const { stdout } = spawnSync('./index.js', ['http://neverssl.com'])
+  assert.ok(stdout.toString().includes("And if the network never redirects you to this page, well as you can see, you're not missing much."))
 }
+
 {
-    const { stderr } = spawnSync('./index.js', []);
-    assert.strictEqual(stderr.toString(), 'Usage: scrape-text <URL>\n');
+  const { stderr } = spawnSync('./index.js', [])
+  assert.strictEqual(stderr.toString(), 'Usage: scrape-text <URL>\n')
 }
