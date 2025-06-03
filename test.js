c509306b1d14bc7cf5e3b26c0cce0b9838d39157
Rich Trott <rtrott@gmail.com>
2025-06-03 12:06:09 -0700

    chore: use more robust test

--- test.js
+++ test.js
@@ -2,8 +2,8 @@ import assert from 'node:assert'
 import { spawnSync } from 'node:child_process'
 
 {
-  const { stdout } = spawnSync('./index.js', ['http://neverssl.com'])
-  assert.ok(stdout.toString().includes("And if the network never redirects you to this page, well as you can see, you're not missing much."))
+  const { stdout } = spawnSync('./index.js', ['http://example.com'])
+  assert.ok(stdout.toString().includes('This domain is for use in illustrative examples in documents.'))
 }
 
 {
